c57961f4d3942d33024b0e25e039c0c8cdb32e62
Qualquer Valor <qualquervalor@gmail.com>
2013-11-18 13:08:47 -0600

    First cut of power ranger and evil ninja

--- powerranger.rb
+++ powerranger.rb
@@ -1,26 +1,71 @@
 class Person
+	attr_reader :caffeine_level
 	def initialize(name,caffeine_level)
 		@name = name
 		@caffeine_level = caffeine_level
 	end
 
 	def run
-		"Move quickly away, try not to draw attention to yourself"
+		"#{@name} moves quickly away, try not to draw attention to yourself"
 	end
 
 	def scream
-		"You make an ear piercingly shrill wail of terror"
+		"#{@name} make an ear piercingly shrill wail of terror"
 	end
 
 	def drink_coffee
 		@caffeine_level +=1;
-		"You feel refreshed and ready to take on the next challenge"
+		"#{@name}  feel refreshed and ready to take on the next challenge"
 	end
 
 end
 
-class PowerRanger
+class PowerRanger < Person
+	def initialize(name,caffeine_level, strength,color)
+		@name = name
+		@caffeine_level = caffeine_level
+		@strength = strength
+		@color = color
+	end
+
+	def punch(person)
+		puts "#{@name} throw a powerful punch"
+		@caffeine_level -=1
+		if @strength > 5
+			person.scream
+			person.run
+		end
+	end
+
+def megazord(person)
+	puts "#{@name} throw a MEGAZORD punch"
+		@caffeine_level -=1
 end
 
-class EvilNinja
+	def rest
+		"#{@name} curl up in a ball and take a brief nap"
+	end
+end
+
+class EvilNinja < Person
+	def initialize(name,caffeine_level,strength,evilness)
+		@name = name
+		@caffeine_level = caffeine_level
+		@strength = strength
+		@evilness = evilness	
+	end
+	
+	def punch(person)
+		puts "#{@name} throw a powerful punch"
+		@caffeine_level -=1
+		if @strength > 5
+			person.scream
+			person.run
+		end
+	end
+
+	def cause_mayhem(person)
+		person.caffeine_level = 0;
+	end
+
 end
