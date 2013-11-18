3c114eb0c7ab41663bd3b169d80f0410b2045b1a
Qualquer Valor <qualquervalor@gmail.com>
2013-11-18 12:56:15 -0600

    Defined intialize, run, scream, and drink_coffee methods on Person

--- powerranger.rb
+++ powerranger.rb
@@ -1,4 +1,22 @@
 class Person
+	def initialize(name,caffeine_level)
+		@name = name
+		@caffeine_level = caffeine_level
+	end
+
+	def run
+		"Move quickly away, try not to draw attention to yourself"
+	end
+
+	def scream
+		"You make an ear piercingly shrill wail of terror"
+	end
+
+	def drink_coffee
+		@caffeine_level +=1;
+		"You feel refreshed and ready to take on the next challenge"
+	end
+
 end
 
 class PowerRanger
